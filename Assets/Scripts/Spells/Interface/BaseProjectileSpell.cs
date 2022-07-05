9587bf8d74f1ba922621be1931ba49d308e3e863
Hoàng Tiến Minh <htminh.1001@gmail.com>
2022-07-05 23:22:51 +0700

    fix: update .gitignore

--- Assets/Scripts/Spells/Interface/BaseProjectileSpell.cs
+++ Assets/Scripts/Spells/Interface/BaseProjectileSpell.cs
@@ -1,46 +1,43 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseProjectileSpell : MonoBehaviour
 {
-    [SerializeField] protected float damage;
-    [SerializeField] protected Collision2D spellCollider;
-    [SerializeField] protected Rigidbody2D rigidBody2D;
-    
-    private Vector2 _screenPosition;
+  [SerializeField] protected float damage;
+  [SerializeField] protected Collision2D spellCollider;
+  [SerializeField] protected Rigidbody2D rigidBody2D;
 
-    private Vector3 _worldPosition;
+  private Vector2 _screenPosition;
 
-    protected void Update()
+  private Vector3 _worldPosition;
+
+  protected void Update()
+  {
+    if (Input.GetMouseButtonDown(0))
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector3 mousePos = Input.mousePosition;
-            _screenPosition = new Vector2(mousePos.x, mousePos.y);
-        }
-
-        _worldPosition = Camera.main.ScreenToWorldPoint(_screenPosition);
+      Vector3 mousePos = Input.mousePosition;
+      _screenPosition = new Vector2(mousePos.x, mousePos.y);
     }
 
-    protected virtual void DamageTarget(Collision2D other) { }
+    _worldPosition = Camera.main.ScreenToWorldPoint(_screenPosition);
+  }
 
-    protected virtual void OnCollisionEnter2D(Collision2D other)
-    {
-        DamageTarget(other);
-    }
+  protected virtual void DamageTarget(Collision2D other) { }
 
-    protected virtual void Shoot(Quaternion angle, float speed, ForceMode2D forceMode2D = ForceMode2D.Impulse)
-    {
-        transform.rotation = angle;   
-        rigidBody2D.SetRotation(angle);
-        rigidBody2D.AddRelativeForce(Vector2.up * speed, forceMode2D);
-        
-    }
+  protected virtual void OnCollisionEnter2D(Collision2D other)
+  {
+    DamageTarget(other);
+  }
 
-    protected virtual void DeSpawn()
-    {
-        Destroy(this.gameObject);
-    }
+  protected virtual void Shoot(Quaternion angle, float speed, ForceMode2D forceMode2D = ForceMode2D.Impulse)
+  {
+    transform.rotation = angle;
+    rigidBody2D.SetRotation(angle);
+    rigidBody2D.AddRelativeForce(Vector2.up * speed, forceMode2D);
+
+  }
+
+  protected virtual void DeSpawn()
+  {
+    Destroy(this.gameObject);
+  }
 }
